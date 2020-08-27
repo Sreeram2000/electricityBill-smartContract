acd808aceee664c476701be1a24deae067e86a79
Sreeram2000 <sreeramshenai2000@gmail.com>
2020-08-27 14:52:23 +0530

    commit 2

--- ElectricityBill.sol
+++ ElectricityBill.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.18 <= 0.6.12;
 
-//Agenda: To deploy a smart contract locally in Ethereum Network in JVM, enable Mega-Mask wallet and to get coins from rickby faucet.
+//Agenda: To deploy a smart contract locally in Ethereum Network in JVM, enable Mega-Mask wallet and to get coins from rickeby faucet.
 
 contract ElectricityBills {
     
@@ -8,21 +8,33 @@ contract ElectricityBills {
     string addrs;
     int    units;
     int    money;
+    string month;
     string consumerid;
     
     
-    function ElectricityBills(string newname, string newaddrs, int newunits, int newmoney, string newconsumerid) public {
+    function ElectricityBills(string newname, string newaddrs, int newunits, int newmoney, string newconsumerid,string newmonth) public {
         
         Name = newname;
         addrs = newaddrs;
         money = newmoney;
         units = newunits;
         consumerid = newconsumerid;
+        month = newmonth;
     }
     
-    function getElectricityBill() public view returns(string,string,int,int,string){
-        return(Name,addrs,units,money,consumerid);
+    function getElectricityBill() public view returns(string,string,int,int,string,string){
+        return(Name,addrs,units,money,consumerid,month);
     }
     
-
+    function setMonthElectricityBill(string newmonth,int newunits, int newmoney) public {
+        money = newmoney;
+        units = newunits;
+        month = newmonth;
+    }
+    
+    
+    // function getMonthElectricityBill() public view returns(string,int,int) {
+    //     return(month,units,money);
+    // }
+    
 }
